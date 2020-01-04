ff14788a02ebb135128570d53780921ca3fe7851
kurian <kurianck.mail@gmail.com>
2020-01-05 00:04:31 +0530

    added erdcit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: "#app",
   data: {
     sections: [{'name':'home','url':'#intro'},{'name':'about','url':'#about'},{'name':'contact','url':'#contact'}],
-    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'}]
+    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'}],
+    venue:'ER& DCI -IT, CDAC TVM'
   }
 })
