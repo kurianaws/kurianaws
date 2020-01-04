5547610ab5c92fb8307a05bb53a783cc8dcb9753
kurian <kurianck.mail@gmail.com>
2020-01-05 00:01:11 +0530

    removed extra

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var app = new Vue({
   el: "#app",
   data: {
     sections: [{'name':'home','url':'#intro'},{'name':'about','url':'#about'},{'name':'contact','url':'#contact'}],
-    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'},{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'line follower'},{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'line follower'},{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'line follower'},{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'line follower'}]
+    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'}]
   }
 })
