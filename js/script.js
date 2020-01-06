572c246db220d834f859170f228bf2a3a38464dd
kurian <kurianck.mail@gmail.com>
2020-01-06 22:14:47 +0530

    added quiz

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: "#app",
   data: {
     sections: [{'name':'home','url':'#intro'},{'name':'about','url':'#about'},{'name':'contact','url':'#contact'}],
-    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'}],
+    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'},
+	{'name':'Quiz','pic':'img/eventpics/quiz.png','url':'quiz.html','title':'Quiz'}],
     venue:'ER& DCI -IT, CDAC TVM'
   }
 })
