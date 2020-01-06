1c7e5fc761821335b3f2a3562752980f446465b9
kurian <kurianck.mail@gmail.com>
2020-01-07 00:05:15 +0530

    added quiz and line follower
        added liner
        uppercase function

--- js/script.js
+++ js/script.js
@@ -2,8 +2,19 @@ var app = new Vue({
   el: "#app",
   data: {
     sections: [{'name':'home','url':'#intro'},{'name':'about','url':'#about'},{'name':'contact','url':'#contact'}],
-    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'linefollower.html','title':'Line Follower'},
+    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'liner.html','title':'Liner'},
 	{'name':'Quiz','pic':'img/eventpics/quiz.png','url':'quiz.html','title':'Quiz'}],
+    show:{'suppourters':'false',
+	'gallery':'false',
+	'schedule':'false',
+    },
     venue:'ER& DCI -IT, CDAC TVM'
-  }
+  },
+    filters: {
+    capitalize: function(value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+   }
 })
