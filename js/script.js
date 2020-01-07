51b3dedae0fb13470df781df34c4be4923c64ea1
kurian <kurianck.mail@gmail.com>
2020-01-07 20:11:07 +0530

    index

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 var app = new Vue({
   el: "#app",
   data: {
-    sections: [{'name':'home','url':'#intro'},{'name':'about','url':'#about'},{'name':'contact','url':'#contact'}],
-    events:[{'name':'liner','pic':'img/eventpics/liner.png','url':'liner.html','title':'Liner'},
-	{'name':'Quiz','pic':'img/eventpics/quiz.png','url':'quiz.html','title':'Quiz'}],
-    show:{'suppourters':'false',
+    sections:{ 
+	'home': {'name':'home',
+	    'url':'#intro',
+	    'show':'true'},
+	'about': {'name':'about',
+	    'url':'#about',
+	    'show':'true'},
+	'events': {'name':'events',
+	    'url':'#events',
+	    'show':'true'},
+	'schedule': {'name':'schedule',
+	    'url':'#schedule',
+	    'show':'false'},
+	'gallery': {'name':'gallery',
+	    'url':'#gallery',
+	    'show':'false'},
+	'supporters': {'name':'supporters',
+	    'url':'#supporters',
+	    'show':'false'},
+	'contact': {'name':'contact',
+	    'url':'#contact',
+	    'show':'false'}
+    },
+    events:[
+	{'name':'liner','pic':'img/eventpics/liner.png','url':'liner.html','title':'Liner'},
+	{'name':'Quiz','pic':'img/eventpics/quiz.png','url':'quiz.html','title':'Quiz'}
+    ],
+    show: {
+	'suppourters':'false',
 	'gallery':'false',
 	'schedule':'false',
-    },
+	'contact':'false'},
     venue:'ER& DCI -IT, CDAC TVM'
   },
     filters: {
